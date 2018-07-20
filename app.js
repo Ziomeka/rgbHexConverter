51426ee10095a569deb6c1c5b830a141006a55f6
Agnieszka Ziomek <poziomkaa@gmail.com>
2018-07-20 14:09:49 +0200

    Tests cleanup, new test and class functionality

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 class Converter {
-    toHex(rgb) {
-        if (!rgb || rgb.length!==3) {
-            throw new Error ('Parameter needed');
-        }
-        const hexed = rgb.reduce((curr, next)=>{
-            return curr = curr + next.toString(16);
-        },"#");
-        return hexed;
+  toHex(rgb) {
+    if (!rgb || rgb.length !== 3) {
+      throw new Error("Parameter needed");
     }
-};
+    if (
+      typeof rgb[0] !== "number" ||
+      typeof rgb[1] !== "number" ||
+      typeof rgb[2] !== "number"
+    ) {
+      throw new Error("Parameter should contain integer numeric data");
+    }
+    if (
+        !Number.isInteger(rgb[0]) ||
+        !Number.isInteger(rgb[1]) ||
+        !Number.isInteger(rgb[2])
+    ) {
+        throw new Error("Parameter should contain integer numeric data");
+      }
+    var canSquash = this.squashable(rgb);
+    const hexed = rgb.reduce((curr, next) => {
+      next = Math.max(Math.min(next, 255), 0);
+      return (curr = curr + (canSquash ? next.toString(16).slice(-1) : ("0" + next.toString(16)).slice(-2)));
+    }, "#");
+    return hexed;
+  }
+  squashable(rgb) {
+    var canSquash = true;
+    rgb.forEach(el => {
+      canSquash = (el % 17 !== 0 && el > 0 && el < 256 ) ? false : canSquash;
+    });
+    return canSquash;
+  }
+}
 
-module.exports = Converter
+module.exports = Converter;
--- app.spec.js
+++ app.spec.js
@@ -1,50 +1,64 @@
 const Converter = require('./app.js');
 
-describe('Hex Konwerter', () => {
+describe('Hex Conwerter', () => {
     let converter;
-    let pattern = /^#\w{3,6}$/
+
     beforeEach(() => {
         converter = new Converter;
     });
 
-    test('Converter exists', () => {
+    test('Should be defined', () => {
         expect(Converter).toBeDefined();
     });
 
-    test('Czy wywolany bez parametow rzuca blad', () => {
+    test('Sould throw error when called without parameter', () => {
         expect(converter.toHex).toThrowError('Parameter needed');
     });
 
-    let badTables = [[],[1], [1,2], [1,2,3,4], [1,2,3,4,5]];
-    badTables.forEach (function(el){
-        test(`Czy zwraca blad jesli tablica nie ma 3 elementow ${el}`, () => {
+    const wrongLengthTables = [[],[1], [1,2], [1,2,3,4], [1,2,3,4,5]];
+    wrongLengthTables.forEach (function(el){
+        test(`Should throw error when called with array with length not equal 3. Test case: [${el}]`, () => {
             expect(function(){
-                converter.toHex([]);
+                converter.toHex(el);
             }
             ).toThrowError('Parameter needed');
         });
     });
 
-    test('Czy zwraca string', () => {
+    const wrongTypeTables = [['a','b','c'], [{r:4}, {g:100}, {b:200}], [3,,3], [100.1, 0, 244]];
+    wrongTypeTables.forEach (function(el){
+        test(`Should throw error when called with array not containing integer numbers. Test case: [${el}]`, () => {
+            expect(function(){
+                converter.toHex(el);
+            }
+            ).toThrowError('Parameter should contain integer numeric data');
+        });
+    });
+
+    test('Should rerurn string', () => {
         expect(converter.toHex([1,2,3])).toEqual(expect.any(String));
     });
 
-    test('Czy zwraca odpowiedni pattern', () => {
-        expect(converter.toHex([1,2,3])).toMatch(pattern);
+    const pattern = /^#\w{3,6}$/
+    test('Should return string begining with # and 3-6 chars', () => {
+        expect(converter.toHex([100,300,300])).toMatch(pattern);
     });
 
     const colors = [
-        {rgb: [0,0,0], value: "#000000"},
-        {rgb: [255,255,255], value: "#ffffff"},
-        {rgb: [100,100,100], value: "#646464"},
-        {rgb: [50,50,50], value: "#323232"},
-        {rgb: [255,50,100], value: "#ff3264"},
-        {rgb: [255,50,0], value: "#ff3200"},
+        {rgb: [100,100,100], value: "#646464", case: 'two digits result for each color component'},
+        {rgb: [50,50,50], value: "#323232", case: 'two digits result for each color component'},
+        {rgb: [255,50,100], value: "#ff3264", case: 'two digits result for each color component'},
+        {rgb: [255,50,0], value: "#ff3200", case: 'single digit result for one color component'},
+        {rgb: [255,11,0], value: "#ff0b00", case: 'single digit result for two color component'},
+        {rgb: [0,0,0], value: "#000", case: 'shortened notation result'},
+        {rgb: [255,255,255], value: "#fff", case: 'shortened notation result'},
+        {rgb: [255,51,0], value: "#f30", case: 'shortened notation result'},
+        {rgb: [300,50,-50], value: "#ff3200", case: 'color compotents out of scale'},
+        {rgb: [300,51,-1], value: "#f30", case: 'color components out of scale and shortened notation'}
       ];
     colors.forEach (function(el){
-        it(`should return ${el.value}, for paremeter ${el.rgb}`, () => {
+        it(`Should return ${el.value}, for parameter [${el.rgb}] testing case - ${el.case}`, () => {
             expect(converter.toHex(el.rgb)).toEqual(el.value);
         })
     });
-
 })
